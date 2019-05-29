739ebe680457170088c0a7516c51c9fa4228e83f
Minoritypanda <48217398+Minoritypanda@users.noreply.github.com>
2019-05-29 18:50:31 -0400

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,175 @@
+<?php
+$catalog=[];
+//Books
+$catalog[101] = [
+ "title" => "The Enemy",
+ "img" => "img/media/theenemy2.jpg",
+ "genre" => "Horror Fiction",
+ "format" => "Paperback",
+ "year" => 2009,
+ "category" => "Books",
+ "authors" => [
+     "Charles Higson",
+ ],
+ "publisher" => "Dell Mass Market Paperback",
+ "isbn" => '978-0156012195'
+];
+
+$catalog[102] = [
+ "title" => "The Wizards Heir",
+ "img" => "img/media/thewizards2.jpg",
+ "genre" => "Fantasy-Magic Books",
+ "format" => "Paperback",
+ "year" => 2010,
+ "category" => "Books",
+ "authors" => [
+     "Cinda William Chima",
+ ],
+ "publisher" => "Disney Hyperion",
+ "isbn" => '978-0061992889'
+];
+
+$catalog[103] = [
+ "title" => "Halts peril",
+ "img" => "img/media/haltsperil2.jpg",
+ "genre" => "Fantasy",
+ "format" => "Paperback",
+ "year" => 2009,
+ "category" => "Books",
+ "authors" => [
+     "John Flannagan",
+ ],
+ "publisher" => "Penguin RAndom House LLC.",
+ "isbn" => '978-1421532431'
+];
+
+$catalog[104] = [
+ "title" => "Grey Wolf Throne",
+ "img" => "img/media/greywolf23.jpg",
+ "genre" => "Fantasy",
+ "format" => "Paperback",
+ "year" => 2012,
+ "category" => "Books",
+ "authors" => [
+     "Cindia William Chima",
+ ],
+ "publisher" => "Disney Hyperion",
+ "isbn" => '978-1449489786'
+];
+//Movies
+$catalog[201] = [
+ "title" => "Your Name",
+ "img" => "img/media/yourname2.jpg",
+ "genre" => "Anime, Drama, Romance",
+ "format" => "DVD",
+ "year" => 2017,
+ "category" => "Movies",
+ "director" => "Makoto Shinkai",
+ "writers" => [
+     "Makoto Shinkai",
+ ],
+ "stars" => [
+     "Mone Kamishiraishi",
+     "Micahel Sinterniklaas",
+     "Aoi Yuki"
+ ]
+];
+
+$catalog[202] = [
+ "title" => "Blade Runner",
+ "img" => "img/media/bladerunner2.jpg",
+ "genre" => "Sci fi, Drama",
+ "format" => "DVD",
+ "year" => 2017,
+ "category" => "Movies",
+ "director" => "Ridley Scott",
+ "writers" => [
+     "Ridley Scott",
+	 
+ ],
+ "stars" => [
+     "Ryan Gosling",
+     "Harrison Ford",
+     "Jared Leto"
+ ]
+];
+
+$catalog[203] = [
+ "title" => "Interstellar",
+ "img" => "img/media/interstellar2.jpg",
+ "genre" => "Sci-fi, Drama",
+ "format" => "DVD",
+ "year" => 2014,
+ "category" => "Movies",
+ "director" => ["Christopher Nolan"],
+ "writers" => [
+     "Christopher Nolan",
+	 "Jonathan Nolan"
+ ],
+ "stars" => [
+     "Matthew McConaughey",
+     "Anne Hathaway",
+     "Jessica Chastain"
+ ]
+];
+
+$catalog[204] = [
+ "title" => "spider man",
+ "img" => "img/media/spider-man.jpg",
+ "genre" => "Animated",
+ "format" => "DVD",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => "Peter Ramsey",
+ "writers" => [
+     "Rodney Rothman",
+	 "Phil Lord"
+ ],
+ "stars" => [
+     "Jake Johnson",
+     "Hailee Steinfeld",
+     "John Mulaney"
+ ]
+];
+
+//Music
+$catalog[301] = [
+ "title" => "Pop Star",
+ "img" => "img/media/popstar2.jpg",
+ "genre" => "Kpop",
+ "format" => "Digital",
+ "year" => 2018,
+ "category" => "Music",
+ "artist" => "Riot Games"
+];
+
+$catalog[302] = [
+ "title" => "Whistle",
+ "img" => "img/media/blackpink2.jpg",
+ "genre" => "Kpop",
+ "format" => "Digital",
+ "year" => 2017,
+ "category" => "Music",
+ "artist" => "Black Pink"
+];
+
+$catalog[303] = [
+ "title" => "I Just",
+ "img" => "img/media/redvelvet2.jpg",
+ "genre" => "KPop",
+ "format" => "Digital",
+ "year" => 2017,
+ "category" => "Music",
+ "artist" => "Red Velvet"
+];
+
+$catalog[304] = [
+ "title" => "Put It Straight",
+ "img" => "img/media/gidle2.jpg",
+ "genre" => "Kpop",
+ "format" => "Digital",
+ "year" => 2018,
+ "category" => "Music",
+ "artist" => "G-idle"
+];
+?>
